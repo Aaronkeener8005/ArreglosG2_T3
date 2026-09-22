67afc075e597ef6fe81fd11c72c106c536d4d0ff
diana evelin lozano <dianaevelinlozano4@gmail.com>
2026-09-22 09:35:53 -0600

    vercion_02:Proyectos en blaco

---
+++ ArreglosG2_T3/Arreglos.logica/MiArreglo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arreglos.logica
+{
+    internal class MiArreglo
+    {
+        private int _tope;
+
+        private int[] _arreglo;
+
+        public MiArreglo(int n)
+        {
+            N = n;
+            _arreglo = new int[N];
+            _tope = 0;
+
+
+        }
+
+        public int N { get; }
+
+        public Boolean EstaVacio => _tope == 0;
+        public Boolean EstaLleno => _tope == N;
+    }
+}
+    
+
